bd572e6601a70de6f7bf80222b16e713f3024eab
Taoufik Boussemousse <114195598+BoussemousseT@users.noreply.github.com>
2024-01-23 00:00:23 -0500

    Add header et footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Header () {
     return (
-        <header>
-            <h1>Header</h1>
+        <header id='header' className='menu-container'>
+            <nav id='nav-bar'>
+                <img src='../../images/ani.jpg' className='menu-icon logo' alt='' />
+            </nav>
         </header>
     )
 }
