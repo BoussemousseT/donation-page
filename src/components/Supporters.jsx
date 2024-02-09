4ac7a1fc0a3c034d8eca1d9e1d8a894070acb791
Taoufik Boussemousse <114195598+BoussemousseT@users.noreply.github.com>
2024-02-09 12:47:16 -0500

    add liens to the supporters

--- src/components/Supporters.jsx
+++ src/components/Supporters.jsx
@@ -9,13 +9,97 @@ function Supporters () {
             <div className='row'>
                 <div className='col-12 col-lg-6'>
                     <h2>Supporters</h2>
-                    <div className='slider-area'>
+                    {/* <div className='slider-area'>
                         <div className='wrapper'>
                             <div className='item'><img alt='' src='../../images/edendonationssupporters.jpg' /></div>
                             <div className='item'><img alt='' src='../../images/edendonationssupporters.jpg' /></div>
                             <div className='item'><img alt='' src='../../images/edendonationssupporters.jpg' /></div>
                             <div className='item'><img alt='' src='../../images/edendonationssupporters.jpg' /></div>
                             <div className='item'><img alt='' src='../../images/edendonationssupporters.jpg' /></div>
+                        </div>
+                    </div> */}
+                    <div className='slider-area'>
+                        <div className='wrapper'>
+                            <div className='item'>
+                                <a href='https://ecoleduconsensusblockchain.com/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/ecole-consensus.png' />
+                                </a>
+                            </div>
+                            <div className='item'>
+                                <a href='https://awfractal.web.app/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/aw-fractal.png' />
+                                </a>
+                            </div>
+                            <div className='item'>
+                                <a href='https://wax.consortium.vote/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/consortium.png' />
+                                </a>
+                            </div>
+                            <div className='item'>
+                                <a href='https://www.wax.io/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/wax.png' />
+                                </a>
+                            </div>
+                            <div className='item'>
+                                <a href='https://ecoleduconsensusblockchain.com/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/ecole-consensus.png' />
+                                </a>
+                            </div>
+                            <div className='item'>
+                                <a href='https://awfractal.web.app/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/aw-fractal.png' />
+                                </a>
+                            </div>
+                            <div className='item'>
+                                <a href='https://wax.consortium.vote/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/consortium.png' />
+                                </a>
+                            </div>
+                            <div className='item'>
+                                <a href='https://www.wax.io/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/wax.png' />
+                                </a>
+                            </div>
+                            <div className='item'>
+                                <a href='https://ecoleduconsensusblockchain.com/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/ecole-consensus.png' />
+                                </a>
+                            </div>
+                            <div className='item'>
+                                <a href='https://awfractal.web.app/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/aw-fractal.png' />
+                                </a>
+                            </div>
+                            <div className='item'>
+                                <a href='https://wax.consortium.vote/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/consortium.png' />
+                                </a>
+                            </div>
+                            <div className='item'>
+                                <a href='https://www.wax.io/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/wax.png' />
+                                </a>
+                            </div>
+                            <div className='item'>
+                                <a href='https://ecoleduconsensusblockchain.com/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/ecole-consensus.png' />
+                                </a>
+                            </div>
+                            <div className='item'>
+                                <a href='https://awfractal.web.app/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/aw-fractal.png' />
+                                </a>
+                            </div>
+                            <div className='item'>
+                                <a href='https://wax.consortium.vote/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/consortium.png' />
+                                </a>
+                            </div>
+                            <div className='item'>
+                                <a href='https://www.wax.io/' target='_blank' rel='noopener noreferrer'>
+                                    <img alt='' src='../../images/wax.png' />
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
